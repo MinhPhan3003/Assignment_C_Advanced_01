1e358cfafd15b2f753d685c62a731208819c3256
MinhPhan <minhphan3003@gmail.com>
2026-09-14 23:10:58 +0700

    Refactor measurement callback functions
    Updated Print_Measurement_Callback to format timestamp and changed sum type in Average_Measurement_Callback to int64_t.

--- Callbacks.c
+++ Callbacks.c
@@ -2,26 +2,30 @@
 
 
 void Print_Measurement_Callback(const Measurement* item) {
-	printf("ID:%u\n", (item->id));
-	printf("status:%u\n", (item->status));
-	printf("timestamp:%d\n", (item)->timestamp);
-	printf("sample count:%zu\n", (item)->sample_count);
+    printf("ID:%u\n", (item->id));
+
+    printf("status:%u\n", (item->status));
+
+    time_t t = (time_t)(item)->timestamp;
+    printf("timestamp %s", ctime(&t));
+
+    printf("sample count:%zu\n", (item)->sample_count);
 }
 
 void Average_Measurement_Callback(const Measurement* item) {
-	const int16_t* p = (item->samples);
-	int16_t sum = 0;
+    const int16_t* p = (item->samples);
+    int64_t sum = 0;
 
-	if (item->sample_count == 0) {
-		return;
-	}
-	for (size_t i = 0; i < (item->sample_count);i++){
-		sum = sum + *(p+i);
-	}
+    if (item == NULL || item->sample_count == 0) {
+        return;
+    }
+    for (size_t i = 0; i < (item->sample_count);i++) {
+        sum = sum + *(p + i);
+    }
 
-	double avg = (double)sum / (item->sample_count);
-	printf("the avg value of ID:%u is %.1f\n",(item->id), avg);
-}	
+    double avg = (double)sum / (double)(item->sample_count);
+    printf("the avg value of ID:%u is %.1f\n", (item->id), avg);
+}
 
 void CheckStatus_Measurement_Callback(const Measurement* item) {
     if (item == NULL) {
@@ -30,9 +34,9 @@ void CheckStatus_Measurement_Callback(const Measurement* item) {
 
     // Checking status flag
     if (item->status == 1) {
-        printf(" Record ID:%u status normal.\n",item->id);
+        printf(" Record ID:%u status normal.\n", item->id);
     }
-    else {        
-        printf(" [ERROR] Record ID:%u ",item->id);
+    else {
+        printf(" [ERROR] Record ID:%u ", item->id);
     }
 }
