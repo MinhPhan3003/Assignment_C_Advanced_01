32f96b49965f2674d0f8a57d596f3f12ef449e03
MinhPhan <minhphan3003@gmail.com>
2026-09-14 23:11:44 +0700

    Enhance Repository functions with ID check and clear
    Added ID check in Repository_Add to prevent duplicates and implemented Repository_Clear to free memory.

--- Repository.c
+++ Repository.c
@@ -1,10 +1,19 @@
-﻿#include "Repository.h"
+#include "Repository.h"
 
-// size stand for number of ptr in current 
+// size stand for number of ptr in current
 bool Repository_Add(Measurement*** storage, size_t* size, size_t* capacity, Measurement* item) {
     if (storage == NULL || size == NULL || capacity == NULL || item == NULL) {
         return false;
     }
+
+    //Check if there is the same created ID 
+    for (size_t i = 0; i < *size; i++) {
+        if ((*(*storage + i))->id == item->id) {
+            printf("[WARNING] ID:%u already exists in storage\n", item->id);
+            return false;
+        }
+    }
+
     size_t new_capacity;
     Measurement** temp;
 
@@ -31,14 +40,14 @@ bool Repository_Add(Measurement*** storage, size_t* size, size_t* capacity, Meas
 
     return true;
 }
-bool Repository_Delete(Measurement*** storage, size_t* size, uint16_t id){
-    if (*storage == NULL || *size == 0) {
+bool Repository_Delete(Measurement*** storage, size_t* size, uint16_t id) {
+    if (storage==NULL|| *storage == NULL || *size == 0) {
         return false;
     }
     for (size_t i = 0; i < *size; i++) {
-        if ((*(*storage + i))!= NULL) {
+        if ((*(*storage + i)) != NULL) {
             if ((*(*storage + i))->id == id) {
-                Measurement_Destroy((*(*storage + i)));
+                Measurement_Destroy((*storage + i));
 
                 //Move all remain records from the delete record position
                 for (size_t j = i; j < (*size - 1); j++) {
@@ -54,32 +63,43 @@ bool Repository_Delete(Measurement*** storage, size_t* size, uint16_t id){
     return false;
 }
 
-Measurement* Repository_Find(Measurement **storage, size_t size, uint16_t id){   
-        if (storage == NULL || size == 0) {
-            printf("FINDING RECORD FAIL");
-            return NULL;
-        }
-
-        else {
-            for (size_t i = 0; i < size; i++)
-            {
-                if ((*(storage + i)) != NULL && (*(storage + i))->id == id) {
-                    printf("There is a record that you need\n");
-                    return *(storage + i);
-                }                   
-            }     
-        }
+Measurement* Repository_Find(Measurement** storage, size_t size, uint16_t id) {
+    if (storage == NULL || size == 0) {
+        printf("FINDING RECORD FAIL");
         return NULL;
+    }
+
+    else {
+        for (size_t i = 0; i < size; i++)
+        {
+            if ((*(storage + i)) != NULL && (*(storage + i))->id == id) {
+                printf("There is a record that you need\n");
+                return *(storage + i);
+            }
+        }
+    }
+    return NULL;
 }
 
 void Repository_ForEach(Measurement** storage, size_t size, void (*callback)(const Measurement*)) {
-    if (storage == NULL || size == 0 ||callback == NULL) {
+    if (storage == NULL || size == 0 || callback == NULL) {
         printf("Invalid input\n");
         return;
-    }  
-    for (size_t i = 0; i < size; i++){
-        if (*(storage + i) != NULL){
+    }
+    for (size_t i = 0; i < size; i++) {
+        if (*(storage + i) != NULL) {
             callback(*(storage + i));
         }
     }
+}
+
+void Repository_Clear(Measurement*** storage, size_t* size, size_t* capacity) {
+    if (storage == NULL || *storage == NULL) return;
+    for (size_t i = 0; i < *size; i++) {
+        Measurement_Destroy(&(*storage)[i]);
+    }
+    free(*storage);
+    *storage = NULL;
+    *size = 0;
+    *capacity = 0;
 }
